2a0cb488a073d6e75c46edd454253ed612140d9d
Narendran <narendran@Narendrans-MacBook-Air.local>
2026-02-04 18:30:54 +0530

    Initial project setup: Docker & Maven configuration

---
+++ src/main/java/com/naren/insurance/swagger/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.naren.insurance.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Insurance Policy Management API",
+                version = "1.0",
+                description = "Enterprise-grade insurance backend"
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+public class OpenApiConfig {}
